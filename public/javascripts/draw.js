0de469d0250585a8b71f1c37b81ec9c75e399c1c
Andrew Jones <ajon542@gmail.com>
2015-05-13 14:45:12 -0700

    Changed colors for comparison
    This still is pretty average but at least it is obvious which ones are
    being compared now.

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -1,18 +1,3 @@
-var items = [];
-
-// Returns an object specifying a semi-random color
-// The color will always have a red value of 0
-// and will be semi-transparent (the alpha value)
-function randomColor() {
-    
-    return {
-        red: 0,
-        green: Math.random(),
-        blue: Math.random(),
-        alpha: (Math.random() * 0.25) + 0.05
-    };
-
-}
 
 function drawRectangle(x, y, width, height, color) {
     var rectangle = new Rectangle(new Point(x, y), new Point(x + width, y + height));
@@ -26,23 +11,27 @@ function drawRectangle(x, y, width, height, color) {
 // Draws a visual representation of a list of values.
 function drawArray(values) {
     
+    // Remove the previously drawn items.
+    project.activeLayer.removeChildren();
+
     var x = 100;
     var y = 100;
     var width = 5;
     
     for (var i = 0; i < values.length; ++i) {
         var color;
-        if (i == currA || i == currB) {
-            color = {
-                red: 1,
-                green: 0,
-                blue: 0,
-                alpha: 1
-            };
-        } else if (i == compA || i == compB) {
+
+        if (i == compA || i == compB) {
             color = {
                 red: 0,
                 green: 1,
+                blue: 0,
+                alpha: 1
+            };
+        } else if (i == currA || i == currB) {
+            color = {
+                red: 1,
+                green: 0,
                 blue: 0,
                 alpha: 1
             };
@@ -55,7 +44,6 @@ function drawArray(values) {
             };
         }
         
-        
         drawRectangle(x, y, width, values[i], color);
         x += width + 2;
     }
@@ -65,6 +53,7 @@ function drawArray(values) {
 }
 
 // Listen for 'drawArray' events.
+var items = [];
 io.on('drawArray', function (data) {
     
     console.log('drawArray event recieved:', data);
@@ -76,7 +65,6 @@ io.on('drawArray', function (data) {
     drawArray(items);
 });
 
-// TODO: We probably want to do a swap and comparison visual display.
 var currA = 0;
 var currB = 0;
 function swap(items, a, b) {
@@ -93,9 +81,6 @@ io.on('swap', function (data) {
     // Log the swap event.
     console.log('swap event recieved');
     
-    // Remove the previously drawn items.
-    project.activeLayer.removeChildren();
-    
     // Swap our local copy of the items so they match the server.
     swap(items, data.a, data.b);
     
@@ -110,12 +95,10 @@ io.on('compare', function (data) {
     // Log the swap event.
     console.log('compare event recieved');
     
-    // Remove the previously drawn items.
-    project.activeLayer.removeChildren();
-    
+    // Update the values being compared.
     compA = data.a;
     compB = data.b;
     
     // Draw the sorted items.
-    drawArray(items);
+    //drawArray(items);
 });
